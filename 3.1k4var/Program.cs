f3990753d9fa09d38592aa1ea3166fa3d966eecb
enoxy <enoxy@DESKTOP-8BHQKRR>
2021-11-11 14:41:43 +0200

    Добавьте файлы проекта.

---
+++ 3.1k4var/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _3._1k4var
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+
+                Console.WriteLine("Введите число x");
+                double x = double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите количество n");
+                int n = int.Parse(Console.ReadLine());
+                double S = 0;
+                for (int i = 1; i <= n; i++)
+                {
+                    S += Math.Sin(i * x) / i;
+                }
+                Console.WriteLine("S={0:F2}", S);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
